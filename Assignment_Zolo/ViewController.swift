982c3e35b956a488eae9769b70cd4a0eece9e139
yatishGautam <yatish.gautam@revv.co.in>
2019-02-07 23:16:12 +0530

    Initial Commit

---
+++ Assignment_Zolo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment_Zolo
+//
+//  Created by Yatish on 07/02/19.
+//  Copyright © 2019 Yatish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
